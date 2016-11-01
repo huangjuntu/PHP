b7c48bdbc595e8ff453380e7b1ea13e7b6fbf054
huangjuntu <1721053474@qq.com>
2016-11-01 17:25:11 +0800

    1.1

---
+++ 1-.php
@@ -0,0 +1,13 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title>字符串</title>
+</head>
+<body>
+<p>
+			
+      <?php echo "hi"."hello"; ?>
+</p>
+</body>
+</html>
