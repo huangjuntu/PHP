a5d497aac935a4bbcc2bd472a7af38a7d1707473
huangjuntu <1721053474@qq.com>
2016-12-07 17:40:41 +0800

    ajax升级版

--- php-ajax/2serverjson.php
+++ php-ajax/2serverjson.php
@@ -1,7 +1,10 @@
 <?php
 //设置页面内容是html编码格式是utf-8
-header("Content-Type: text/plain;charset=utf-8"); 
-//header("Content-Type: application/json;charset=utf-8"); 
+//header("Content-Type: text/plain;charset=utf-8"); 
+header('Access-Control-Allow-Origin:*');
+header('Access-Control-Allow-Methods:POST,GET');
+header('Access-Control-Allow-Credentials:true'); 
+header("Content-Type: application/json;charset=utf-8"); 
 //header("Content-Type: text/xml;charset=utf-8"); 
 //header("Content-Type: text/html;charset=utf-8"); 
 //header("Content-Type: application/javascript;charset=utf-8"); 
@@ -17,7 +20,7 @@ $staff = array
 //判断如果是get请求，则进行搜索；如果是POST请求，则进行新建
 //$_SERVER是一个超全局变量，在一个脚本的全部作用域中都可用，不用使用global关键字
 //$_SERVER["REQUEST_METHOD"]返回访问页面使用的请求方法
-if ($_SERVER["REQUEST_METHOD"] == "GET") {
+if ($_SERVER["REQUEST_METHOD"] == "GET") {//request_method
 	search();
 } elseif ($_SERVER["REQUEST_METHOD"] == "POST"){
 	create();
