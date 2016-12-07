2e1da29e6a3a1a825dc49738baf9e0ded9824993
huangjuntu <1721053474@qq.com>
2016-12-07 16:55:55 +0800

    ajax

---
+++ test.php
@@ -0,0 +1,13 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title>test</title>
+</head>
+<body>
+<h1>这是一个测试页面</h1>
+<?php
+    echo "helloworld";
+?>
+</body>
+</html>
